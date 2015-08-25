b217880a4a3058e0b03f5556c8084f919a7e4037
hsjeon1224 <hsjeon1224@inbiznetcorp.com>
2015-08-25 18:12:05 +0900

    채팅 수정중..

--- cat/mobile2.js
+++ cat/mobile2.js
@@ -1,15 +1,10 @@
 /**
  * New node file
  */
-/**
- * New node file
- */
 var http 		= require("http")
 	,fs 		= require("fs")
 	,sockeio	= require("socket.io");
 
-//현재 클라이언트의
-var    socketRoom    =    {};
 //웹서버를 만듭니다.
 var server  = http.createServer(function(request, response) {
 	//htmlPage.html 파일을 읽습니다.
@@ -17,18 +12,18 @@ var server  = http.createServer(function(request, response) {
 		response.writeHead(200, {"Content-Type":"text/html"});
 		response.end(data);
 	})
-}).listen(52273,function(){
+}).listen(52273,function() {
 	console.log("Server running at http://127.0.0.1:52273");
 });
 
 //소켓 서버를 만듭니다.
 var io	= sockeio.listen(server);
 io.set("log level", 2);
-io.sockets.on("connection",function(socket){
-	console.log("사용자가 접근하였습니다.");
+io.sockets.on("connection",function(socket) {
+	console.log("사용자가 접근하였습니다.|"+socket.id);
 
 	//클라이언트 - > 서버 방목록 조회 요청
-	socket.on("requestRandomChat",function(data){
+	socket.on("requestRandomChat",function(data) {
 		var    rooms    =    io.sockets.manager.rooms;
 
 		//혼자인 방을 조회후 매치
@@ -43,43 +38,67 @@ io.sockets.on("connection",function(socket){
 			if(rooms[key].length  == 1) {
 				var    roomKey    =    key.replace('/', '');
 				socket.join(roomKey);
+				socket.set("name",roomKey);
 				io.sockets.to(roomKey).emit("completeMatch",{});
-				socketRoom[socket.id]    =    roomKey;
 				return;
 			}
         }//for end
 
+		console.log(socket.id);
+
 		//빈방이 없을 경우
 		socket.join(socket.id);
-		socketRoom[socket.id]    =    roomKey;
+		socket.set("name",socket.id);
 
 	});
 	// end 클라이언트 - > 서버 방목록 조회 요청
 
 	//클라이언트 -> 서버 취소 요청
 	socket.on("cancelRequestRandomChat", function() {
-		socket.leave(socketRoom[socket.id]);		//방나가기
+		socket.get("name",function(error, data) {
+		    socket.leave(data);
+			io.sockets.to(data).emit("disconnectClient")		//서버 -> 클라이언트 disconnect 이벤트 요청
+		});
 	});
 	//end 클라이언트 -> 서버 취소 요청
 
 	//클라이언트 -> 서버 Message 요청
 	socket.on("sendMessage", function(data) {
-	    io.sockets.to(socketRoom[socket.id]).emit("receiveMessage",data);
+		socket.get("name",function(error, data) {
+			console.log("sendMessage|"+data);
+		});
+	    io.sockets.to(data).emit("receiveMessage",data);
 	});
 	//end 클라이언트 -> 서버 Message 요청
 
 	//클라이언트 -> 서버 연결해제 요청시
-	socket.on("disconnectClienth", function(data) {
-	    var roomKey    =    socketRoom[socket.id];
-	    socket.leave(roomKey);						   			//방나가기
-	    io.sockets.to(roomKey).emit("disconnectClienth")		//서버 -> 클라이언트 disconnect 이벤트 요청
-	    var clients    =    io.sockets.clients(roomKey)			//현재 룸에 있는 클라언트들의 목록 조회
-	    for (var i = 0; i < clients.length; i++) {				//현재 방에 있는 모든 클라이언트를 방에서 나가게 한다.
-	    	clients[i].leave(roomKey);
-		}
+	socket.on("disconnectClient", function(data) {
+		socket.get("name",function(error, data) {
+			socket.leave(data);
+			io.sockets.to(data).emit("disconnectClient")		//서버 -> 클라이언트 disconnect 이벤트 요청
+		});
 	});
 	//end 클라이언트 -> 서버 연결해제 요청시
 
+	//클라이언트 -> 서버 현재 방정보 조회
+	socket.on("requestClientListInRoom", function(data) {
+		var    rooms    =    io.sockets.manager.rooms;
+		for(var key in rooms) {
+
+			console.log("key : " +key);
+			if(key == '') {
+                continue;
+            }
+
+			console.log("-----------------------------------------");
+			console.log("nameRoom:"+rooms[key]);
+			console.log("keyRoom:"+key.replace('/', ''));
+			console.log("cnt:"+rooms[key].length);
+			console.log("-----------------------------------------");
+
+        }//for end
+	});
+	//end 클라이언트 -> 서버 현재 방정보 조회
 
 });
 
